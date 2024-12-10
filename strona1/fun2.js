1e8999a837dc39184c22820739a2eea84b4c72e8
Bartosz Wiaderny <10376794u@qed.local>
2024-12-10 12:52:56 +0100

    dod

--- strona1/fun2.js
+++ strona1/fun2.js
@@ -12,9 +12,33 @@ function myFunction() {
 //galeria z zdjęciami
 
 var zdjecia=document.querySelectorAll(".img");
-zdjecia.forEach(item=>{
-    item.addEventListener("mouseover",()=>{
-        console.log("click")
-        
+var span=document.querySelectorAll("span")
+
+for(let i = 0;i<zdjecia.length;i++){
+    zdjecia[i].addEventListener("mouseover",()=>{
+        console.log(i);
+        let node=zdjecia[i].childNodes
+        node.forEach(item=>{
+            span.forEach(item2=>{
+                if(item.id==item2.id){
+                    let elem=item2
+                    elem.style.visibility="visible";
+                    console.log(elem)
+                }
+            })
+        })
     })
-})
+    zdjecia[i].addEventListener("mouseout",()=>{
+        console.log(i);
+        let node=zdjecia[i].childNodes
+        node.forEach(item=>{
+            span.forEach(item2=>{
+                if(item.id==item2.id){
+                    let elem=item2
+                    elem.style.visibility="hidden";
+                    console.log(elem)
+                }
+            })
+        })
+    })
+}
